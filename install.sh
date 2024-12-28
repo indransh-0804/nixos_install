66600837febe4aee1a0146066359824e4f60e4a4
indransh-0804 <indransh.0804.sharma@gmail.com>
2024-12-28 21:52:45 +0530

    Adjusted for LVM

--- install.sh
+++ install.sh
@@ -1,28 +1,40 @@
 #!/usr/bin/env bash
 
+# Variables
+DISK="/dev/sda"      # Replace with your target disk
+
+#script
+
+echo "The Script Begins..."
 set -e  # Exit on error
 set -x  # Print commands as they run (useful for debugging)
 
-# Variables
-DISK="/dev/sda"      # Replace with your target disk
-HOSTNAME="mynix"  # Change to your desired hostname
+echo "Creating LVM ..."
+sudo pvcreate "${DISK}"2
+sudo vgcreate NixMain "${DISK}"2
+sudo lvcreate -L8G NixMain -n swap
+sudo lvcreate -L64G NixMain -n cryptRoot
+sudo lvcreate -l 100%FREE NixMain -n cryptHome
 
-#script begins ...
-sudo cryptsetup -v -y -c aes-xts-plain64 -s 512 -h sha512 -i 2000 --use-random --label=NIX_LUKS luksFormat --type luks2 "${DISK}2"
-sudo cryptsetup open --type luks "${DISK}"2 cryptMain
-sudo pvcreate /dev/mapper/cryptMain
-sudo vgcreate Main /dev/mapper/cryptMain
-sudo lvcreate -L2G Main -n swap
-sudo lvcreate -L64G Main -n root
-sudo lvcreate -l 100%FREE Main -n home
+echo "Encrypting lv ..."
+sudo cryptsetup -v -y -c aes-xts-plain64 -s 512 -h sha512 -i 2000 --use-random --label=NIXLUKS_ROOT luksFormat --type luks2 /dev/NixMain/cryptRoot
+sudo cryptsetup open --type luks /dev/NixMain/cryptRoot NixRoot
+sudo cryptsetup -v -y -c aes-xts-plain64 -s 512 -h sha512 -i 2000 --use-random --label=NIXLUKS_HOME luksFormat --type luks2 /dev/NixMain/cryptHome
+sudo cryptsetup open --type luks /dev/NixMain/cryptHome NixHome
+
+echo "Making FileSystem ..."
 sudo mkfs.fat  -n NIX_BOOT -F32 "${DISK}"1
-sudo mkfs.ext4 -L NIX_ROOT /dev/mapper/Main-root
-sudo mkfs.ext4 -L NIX_HOME /dev/mapper/Main-home
-sudo mkswap -L NIX_SWAP /dev/mapper/Main-swap
+sudo mkfs.ext4 -L NIX_ROOT /dev/mapper/NixMain-NixRoot
+sudo mkfs.ext4 -L NIX_HOME /dev/mapper/NixMain-NixHome
+sudo mkswap -L NIX_SWAP /dev/mapper/NixMain-swap
+
+echo "Mounting Disk ..."
 sudo mount /dev/disk/by-label/NIX_ROOT /mnt
 sudo mkdir /mnt/boot
 sudo mkdir /mnt/home
 sudo mount -o umask=0077 /dev/disk/by-label/NIX_BOOT /mnt/boot
 sudo mount /dev/disk/by-label/NIX_HOME /mnt/home
 sudo swapon -L NIX_SWAP
+
+echo "Finished_"
 lsblk
