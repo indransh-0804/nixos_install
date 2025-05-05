2c4b8c60eacb5b738b6dcd7bb7c74e40dfe9c8c8
Indransh Harikesh Sharma <indransh.0804.sharma@gmail.com>
2025-05-05 15:44:19 +0530

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,62 +1,55 @@
 #!/usr/bin/env bash
 
-# Variables
-DISK="/dev/sda"      # Replace with your target disk
-
-#script
-
 echo "The Script Begins..."
 set -e  # Exit on error
 set -x  # Print commands as they run (useful for debugging)
 
-parted "$DISK" -- mklabel gpt
-parted "$DISK" -- mkpart primary 512MiB 100%
-parted "$DISK" -- mkpart ESP fat32 1MiB 512 MiB
-parted "$DISK" -- set 2 boot on
-
 echo "Creating LVM ..."
-sudo pvcreate "${DISK}"2
-sudo vgcreate NixMain "${DISK}"2
-sudo lvcreate -L8G NixMain -n swap
-sudo lvcreate -L64G NixMain -n cryptRoot
-sudo lvcreate -l 100%FREE NixMain -n cryptHome
+sudo pvcreate nvme0n1p7
+sudo vgcreate ArchMain nvme0n1p7
+sudo lvcreate -L 8G ArchMain -n Swap
+sudo lvcreate -L 64G ArchMain -n cryptRoot
+sudo lvcreate -l 100%FREE ArchMain -n cryptHome
 
 echo "Encrypting lv ..."
-sudo cryptsetup -v -y -c aes-xts-plain64 -s 512 -h sha512 -i 2000 --use-random --label=NIXLUKS_ROOT luksFormat --type luks2 /dev/NixMain/cryptRoot
-sudo cryptsetup -v -y -c aes-xts-plain64 -s 512 -h sha512 -i 2000 --use-random --label=NIXLUKS_HOME luksFormat --type luks2 /dev/NixMain/cryptHome
-sudo cryptsetup open --type luks /dev/NixMain/cryptRoot NixRoot
-sudo cryptsetup open --type luks /dev/NixMain/cryptHome NixHome
+sudo cryptsetup -v -y -c aes-xts-plain64 -s 512 -h sha512 -i 2000 --use-random --label=ARCH_ROOT luksFormat --type luks2 /dev/ArchMain/cryptRoot
+sudo cryptsetup -v -y -c aes-xts-plain64 -s 512 -h sha512 -i 2000 --use-random --label=ARCH_HOME luksFormat --type luks2 /dev/ArchMain/cryptHome
+sudo cryptsetup open --type luks /dev/ArchMain/cryptRoot ArchRoot
+sudo cryptsetup open --type luks /dev/ArchMain/cryptHome ArchHome
 
 echo "Making FileSystem ..."
-sudo mkfs.fat  -n NIX_BOOT -F32 "${DISK}"1
-sudo mkswap -L NIX_SWAP /dev/mapper/NixMain-swap
-sudo mkfs.btrfs -L NIX_ROOT /dev/mapper/NixRoot
-sudo mkfs.btrfs -L NIX_HOME /dev/mapper/NixHom
+sudo mkfs.fat  -n ARCH_BOOT -F32 nvme0n1p5
+sudo mkfs.fat  -n ARCH_EFI -F32 nvme0n1p6
+sudo mkswap -L ARCH_SWAP /dev/ArchMain/Swap
+sudo mkfs.btrfs -L ARCH_ROOT /dev/mapper/ArchRoot
+sudo mkfs.btrfs -L ARCH_HOME /dev/mapper/ArchHome
 
 echo "Making btrfs Subvolume ..."
-sudo mount -t btrfs /dev/disk/by-label/NIX_ROOT /mnt
+
+sudo mount -t btrfs /dev/mapper/ArchRoot /mnt
 cd /mnt
-sudo btrfs subvolume create @
-sudo btrfs subvolume create @nix
-sudo btrfs subvolume create @log
+sudo btrfs subvolume create @root
+sudo btrfs subvolume create @pkg
+sudo btrfs subvolume create @cache
 sudo btrfs subvolume create @.snap
 cd 
+umount /mnt
+sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@root /dev/mapper/ArchRoot /mnt
+sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@cache --mkdir /dev/mapper/ArchRoot /mnt/var/cache
+sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@pkg --mkdir /dev/mapper/ArchRoot /mnt/var/lib/pacman
+sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@.snap --mkdir /dev/mapper/ArchRoot /mnt/.snap
 
-sudo mount -t btrfs --mkdir /dev/disk/by-label/NIX_HOME /mnt/home
+sudo mount -t btrfs --mkdir /dev/mapper/ArchHome /mnt/home
 cd /mnt/home
 sudo btrfs subvolume create @home
 cd
-sudo umount -R /mnt
+sudo umount -R /mnt/home
+sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@home --mkdir /dev/mapper/ArchHome /mnt/home
 
-echo "Mounting Disk ..."
-sudo mount -o umask=0077 --mkdir /dev/disk/by-label/NIX_BOOT /mnt/boot
-sudo swapon -L NIX_SWAP
-sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@ --mkdir /dev/disk/by-label/NIX_ROOT /mnt
-sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@nix --mkdir /dev/disk/by-label/NIX_ROOT /mnt/etc/nix
-sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@log --mkdir /dev/disk/by-label/NIX_ROOT /mnt/var/log
-sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@.snap --mkdir /dev/disk/by-label/NIX_ROOT /mnt/.snap
-sudo mount -t btrfs -o rw,ssd,noatime,compress=zstd,discard=async,subvol=@home --mkdir /dev/disk/by-label/NIX_HOME /mnt/home
+sudo mount -o umask=0077 --mkdir /dev/nvme0n1p5 /mnt/boot
+sudo mount -o umask=0077 --mkdir /dev/nvme0n1p6 /mnt/boot/efi
+sudo swapon /dev/ArchMain/Swap
+sudo swapon -a
 
-echo "Finished_"
 lsblk
-nixos-generate-config --root /mnt
+
